788860cdb9e09b0589a1fff2435632efca3baa64
Carmine Margiotta <cmargiotta@posteo.net>
2023-01-16 14:26:37 +0100

    Tokenized numbers inside braces

--- src/tokenizer.hpp
+++ src/tokenizer.hpp
@@ -8,6 +8,41 @@
 
 namespace e_regex
 {
+    template<typename matches, char... string>
+    struct extract_braces_numbers;
+
+    template<char... current, char head, char... tail>
+    struct extract_braces_numbers<std::tuple<pack_string<current...>>, head, tail...>
+        : public extract_braces_numbers<pack_string<current..., head>, tail...>
+    {
+            // Populating first number
+    };
+
+    template<typename first, char... tail>
+    struct extract_braces_numbers<std::tuple<first>, '-', tail...>
+        : public extract_braces_numbers<std::tuple<first, pack_string<'-'>>, tail...>
+    {
+            // '-' found, first number done
+    };
+
+    template<char... current, typename first, typename second, char head, char... tail>
+    struct extract_braces_numbers<std::tuple<first, second, pack_string<current...>>, head, tail...>
+        : public extract_braces_numbers<std::tuple<first, second, pack_string<current..., head>>, tail...>
+    {
+            // Populating second number
+    };
+
+    template<typename match, char... tail>
+    struct extract_braces_numbers<match, '}', tail...>
+    {
+            // '}' found, finishing
+            using numbers   = match;
+            using remaining = pack_string<tail...>;
+    };
+
+    template<char... string>
+    using extract_braces_numbers_t = extract_braces_numbers<std::tuple<pack_string<>>, string...>;
+
     template<typename current, typename string>
     struct tokenizer;
 
@@ -25,6 +60,15 @@ namespace e_regex
             // Escaped character
             using current_ = tuple_cat_t<current, std::tuple<pack_string<'\\', head>>>;
             using tokens   = typename tokenizer<current_, pack_string<tail...>>::tokens;
+    };
+
+    template<typename current, char... tail>
+    struct tokenizer<current, pack_string<'{', tail...>>
+    {
+            // Number inside braces
+            using numbers  = extract_braces_numbers_t<tail...>;
+            using current_ = tuple_cat_t<current, typename numbers::numbers>;
+            using tokens   = typename tokenizer<current_, typename numbers::remaining>::tokens;
     };
 
     template<typename current>
