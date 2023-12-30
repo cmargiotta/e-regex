564fcf789dac74f3ab45b9af5ccf7cef48ceda51
Carmine Margiotta <carmine.margiotta@wsense.it>
2023-12-30 22:29:16 +0100

    Update deps

--- flake.nix
+++ flake.nix
@@ -1,46 +1,24 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    devshell.url = "github:numtide/devshell";
     mach-nix.url = "mach-nix/3.5.0";
   };
-  outputs = { self, nixpkgs, flake-utils, devshell, mach-nix }:
+  outputs = { self, nixpkgs, flake-utils, mach-nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ devshell.overlay ];
-          config.allowUnfree = true;
         };
       in
       rec {
-        nixConfig.sandbox = "relaxed";
-        devShell = pkgs.devshell.mkShell {
-          name = "e-regex";
-
-          commands = [
-            {
-              name = "build";
-              help = "Automatically configure build folder and run build";
-              command = "meson setup build --buildtype=debug; meson compile -C build";
-            }
-            {
-              name = "run_tests";
-              help = "Run tests";
-              command = "ninja -C build test";
-            }
-          ];
-
-          env = [
-          ];
-
-          packages =
-            with pkgs;
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs;
             [
               meson
               ninja
-              clang_15
+              clang_17
+              clang-tools_17
 
               (
                 mach-nix.lib."${system}".mkPython {
@@ -50,6 +28,8 @@
                 }
               )
             ];
+
+          nativeBuildInputs = with pkgs; [ pkg-config ];
         };
       });
 }
