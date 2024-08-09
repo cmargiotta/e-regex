45124ccbfaec85bdd6bca2a8600c598a2088fb95
Carmine Margiotta <email@cmargiotta.net>
2024-08-09 13:31:43 +0200

    Fixed specialization warning

--- src/utilities/balanced.hpp
+++ src/utilities/balanced.hpp
@@ -8,49 +8,49 @@ namespace e_regex
     template<unsigned round_par, unsigned square_par, unsigned braces, typename... strings>
     struct balanced;
 
-    template<auto round_par, auto square_par, auto braces, typename... strings>
+    template<unsigned round_par, unsigned square_par, unsigned braces, typename... strings>
     struct balanced<round_par, square_par, braces, pack_string<'('>, strings...>
         : public balanced<round_par + 1, square_par, braces, strings...>
     {
     };
 
-    template<auto round_par, auto square_par, auto braces, typename... strings>
+    template<unsigned round_par, unsigned square_par, unsigned braces, typename... strings>
     struct balanced<round_par, square_par, braces, pack_string<')'>, strings...>
         : public balanced<round_par - 1, square_par, braces, strings...>
     {
     };
 
-    template<auto round_par, auto square_par, auto braces, typename... strings>
+    template<unsigned round_par, unsigned square_par, unsigned braces, typename... strings>
     struct balanced<round_par, square_par, braces, pack_string<'['>, strings...>
         : public balanced<round_par, square_par + 1, braces, strings...>
     {
     };
 
-    template<auto round_par, auto square_par, auto braces, typename... strings>
+    template<unsigned round_par, unsigned square_par, unsigned braces, typename... strings>
     struct balanced<round_par, square_par, braces, pack_string<']'>, strings...>
         : public balanced<round_par, square_par - 1, braces, strings...>
     {
     };
 
-    template<auto round_par, auto square_par, auto braces, typename... strings>
+    template<unsigned round_par, unsigned square_par, unsigned braces, typename... strings>
     struct balanced<round_par, square_par, braces, pack_string<'{'>, strings...>
         : public balanced<round_par, square_par, braces + 1, strings...>
     {
     };
 
-    template<auto round_par, auto square_par, auto braces, typename... strings>
+    template<unsigned round_par, unsigned square_par, unsigned braces, typename... strings>
     struct balanced<round_par, square_par, braces, pack_string<'}'>, strings...>
         : public balanced<round_par, square_par, braces - 1, strings...>
     {
     };
 
-    template<auto round_par, auto square_par, auto braces, typename head, typename... strings>
+    template<unsigned round_par, unsigned square_par, unsigned braces, typename head, typename... strings>
     struct balanced<round_par, square_par, braces, head, strings...>
         : public balanced<round_par, square_par, braces, strings...>
     {
     };
 
-    template<auto round_par, auto square_par, auto braces>
+    template<unsigned round_par, unsigned square_par, unsigned braces>
     struct balanced<round_par, square_par, braces>
     {
             static constexpr bool value = round_par == 0 && square_par == 0 && braces == 0;
